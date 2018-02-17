8243aa1ff4b1ef7162fafb251181fb5a9c9c5fc9
Aaron Burke <aaron@aburke.me>
2018-02-17 13:24:35 +1100

    Fixes meta.js

--- meta.js
+++ meta.js
@@ -161,7 +161,7 @@ module.exports = {
   filters: {
     'config/test.env.js': 'unit || e2e',
     'build/webpack.test.conf.js': "unit && runner === 'karma'",
-    'test/unit/specs/HelloWorld.spec.js': 'unit && runner !== karma',
+    'test/unit/specs/HelloWorld.spec.js': "unit && runner !== 'karma'",
     'test/unit/specs/HelloWorld.spec.coffee': 'unit && runner === karma',
     'test/unit/index.js': "unit && runner === 'karma'",
     'test/unit/jest.conf.js': "unit && runner === 'jest'",
